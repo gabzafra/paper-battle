296c224f024b84d2b7d2bb79ccee347d9c07c137
gabriel <gab.zafra@gmail.com>
2019-11-05 10:33:06 +0100

    deck shuffle method

--- src/deck.js
+++ src/deck.js
@@ -1,7 +1,20 @@
-class Deck{
-    constructor(id,name,cardsArray){
+class Deck {
+    constructor(id, name, cardsArray) {
         this.id = id;
         this.name = name;
         this.cardsArray = cardsArray;
+        this.shuffleCards();
+    }
+
+    shuffleCards() {
+        let currentIndex = this.cardsArray.length,
+            temporaryValue, randomIndex;
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            temporaryValue = this.cardsArray[currentIndex];
+            this.cardsArray[currentIndex] = this.cardsArray[randomIndex];
+            this.cardsArray[randomIndex] = temporaryValue;
+        }
     }
 }
